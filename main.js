f336a90e34a95417dadde6294ff8d301128cf134
Dora Budić <dbudic0706@gmail.com>
2021-04-09 16:56:31 +0200

    1st part

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
-
+    albums: [],
+  },
+  mounted() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((response) => {
+        this.albums = response.data.response;
+      });
   },
   methods: {
 
