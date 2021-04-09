a006dca1f30649829e8b199d19b0692a2d8a5633
Dora Budić <dbudic0706@gmail.com>
2021-04-09 18:41:29 +0200

    Bonus

--- main.js
+++ main.js
@@ -4,14 +4,31 @@ var app = new Vue({
   el: '#root',
   data: {
     albums: [],
+    genres: ['All'],
+    selected: 'All',
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((response) => {
         this.albums = response.data.response;
       });
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((response) => {
+        this.albums.forEach((item, i) => {
+          if (!this.genres.includes(item.genre)) {
+            this.genres.push(item.genre);
+          }
+        });
+
+      });
   },
   methods: {
-
-  }
+    currentGenre: function () {
+      if (this.selected == 'All') {
+        return '';
+      } else {
+        return this.selected;
+      }
+    }
+   }
 });
