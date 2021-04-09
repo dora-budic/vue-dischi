ee812ee77e7b67b1c59593d011d6be8a6eb567a9
Dora Budić <dbudic0706@gmail.com>
2021-04-09 19:38:37 +0200

    Bonus 2

--- main.js
+++ main.js
@@ -6,20 +6,19 @@ var app = new Vue({
     albums: [],
     genres: ['All'],
     selected: 'All',
+    filter: 'sort',
+    albumsAscending: [],
+    albumsDescending: []
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((response) => {
         this.albums = response.data.response;
-      });
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-      .then((response) => {
         this.albums.forEach((item, i) => {
           if (!this.genres.includes(item.genre)) {
             this.genres.push(item.genre);
           }
         });
-
       });
   },
   methods: {
@@ -30,5 +29,20 @@ var app = new Vue({
         return this.selected;
       }
     }
-   }
+  },
+  computed: {
+    albumsComputed: function () {
+      if (this.filter == 'min-year') {
+        this.albumsAscending = [...this.albums];
+        this.albumsAscending.sort(function(a,b) {return a.year - b.year;})
+        return this.albumsAscending;
+      } else if (this.filter == 'max-year') {
+        this.albumsDescending = [...this.albums];
+        this.albumsDescending.sort(function(a,b) {return b.year - a.year;})
+        return this.albumsDescending;
+      } else {
+        return this.albums;
+      }
+    }
+  }
 });
